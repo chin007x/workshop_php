0f7a94c233b6f29fdaba15682c497767ca0725ff
chin007x <chinnawatq@gmail.com>
2022-01-18 11:26:15 +0700

    first commit

--- layout/sidebar.php
+++ layout/sidebar.php
@@ -3,7 +3,7 @@
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
       <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">Admin Panel</span>
+      <span class="brand-text font-weight-light">Nova2</span>
     </a>
     <!-- Sidebar -->
     <div class="sidebar">
